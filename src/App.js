31180ecc094334046038d581f6d782aeb878d745
Mickael Brothier <mickaelbrothier@outlook.com>
2023-04-17 19:07:32 +0200

    Début de création des routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import Home from "./components/Home/Home";
+import Location from "./components/Location/LocationList";
+import Apropos from './components/Apropos/Apropos';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/LocationList/:id' element={<Location/>}/>
+        <Route path='/Apropos' element={<Apropos/>}/>
+      </Routes>
     </div>
   );
 }
