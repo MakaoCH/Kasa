3bc9f785aca808ea3c57ba82dee4df664e22cf66
Mickael Brothier <mickaelbrothier@outlook.com>
2023-04-21 15:58:35 +0200

    Modification A propos

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -3,11 +3,15 @@ import arrow from '../../assets/arrow.png';
 import styles from './Collapse.module.css';
 
 const Collapse = ({title, content}) => {
+
   const [toggle, setToggle] = useState(false);
 
   return(
+
     <div className={styles.collapse}  id={`Collapse-${title}`}>
+        
       <div className={styles.collapseHeader}>
+
         <div className={styles.collapseTitle}>{title}</div>
             <span className={styles.collapseArrow}  onClick={() => setToggle(!toggle)}>
             <img className={toggle ? styles.rotateArrow : ''} src={arrow} alt="Ouvrir cette liste"/>
