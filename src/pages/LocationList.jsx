d7d9909fbd527c5e71b6a83a24942782dd05641d
Mickael Brothier <mickaelbrothier@outlook.com>
2023-04-25 18:52:15 +0200

    Collapse fiche logement

--- src/pages/LocationList.jsx
+++ src/pages/LocationList.jsx
@@ -5,11 +5,12 @@ import data from '../utils/logements.json';
 import Rating from '../components/Rating/Rating';
 import Host from '../components/Host/Host';
 import Tags from '../components/Tags/Tags';
+import Collapse from '../components/Collapse/Collapse';
 
 const LocationsList = () => {
   const { id } = useParams();
   const accommodation = data.find(item => item.id === id) || {};
-  const { pictures = [], title = "", location = "", rating = null, tags = [] } = accommodation;
+  const { pictures = [], title = "", location = "", rating = null, tags = [], description = {}, equipments = [] } = accommodation;
   const hostData = accommodation.host || {};
 
   return (
@@ -20,6 +21,10 @@ const LocationsList = () => {
       <Tags tags={tags} />
       {rating && <Rating rating={parseInt(rating)} />}
       <Host name={hostData.name} picture={hostData.picture} />
+      
+      <Collapse title="Description" content={description} />
+      <Collapse title="Equipements" content={equipments} />   
+
       
     </div>
   );
